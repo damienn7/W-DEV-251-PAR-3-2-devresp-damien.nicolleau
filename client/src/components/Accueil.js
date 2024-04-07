a5aabea1357343099f5412ef71e121f1efdab7b0
Mehdi <mehdi1.zenati@epitech.eu>
2024-04-07 17:48:38 +0200

    removed useless stuff

--- client/src/components/Accueil.js
+++ client/src/components/Accueil.js
@@ -1,21 +1,14 @@
 import '../style/Unique.css';
 import '../style/Accueil.css'
-import Button from '@mui/material/Button';
 import Header from './Header'
 import Footer from './Footer'
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Box from '@mui/material/Box';
-import Video from '../assets/Julien.mp4'
-import Pub from '../assets/Pub.png'
-import Carousel from 'react-material-ui-carousel'
 import MCarousel from './Carousel/Carousel.js'
 import Table from './Table/Table'
-import { Typography } from '@mui/material';
 import { useLocation } from "react-router-dom";
 import BreadcrumbsComponent from "./breadcrumbs";
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import LoadingImages from './LoadingImages.jsx';
 
 // import {result,noItems,price,countItem,orderId,articlesPanier,setResult,setNoItems,setPrice,setCountItem,setOrderId,setArticlesPanier,calcPrice,calcQuantity} from './StatePanier';
 
@@ -35,7 +28,6 @@ const calcQuantity = (id) => {
     axios
       .get(`http://localhost:8000/api/count_item/${id}`)
       .then((response) => {
-        // console.table(response.data['quantity'][0]['count']);
         setCountItem(response.data['quantity'][0]['count']);
       })
       .catch((error) => {
@@ -56,22 +48,6 @@ const calcQuantity = (id) => {
     return price_calc;
   }
 
-  const decryptData = async (encryptedData, key) => {
-    const decryptedData = await crypto.subtle.decrypt({ name: 'AES-GCM', iv: new Uint8Array(12) }, key, encryptedData);
-  
-    const textDecoder = new TextDecoder();
-    return textDecoder.decode(decryptedData);
-  };
-  // const storedEncryptedData = localStorage.getItem('encryptedAdmin');
-  //        if (storedEncryptedData) {
-  //         const key = localStorage.getItem('encryptedAdmin');
-  //          decryptData(new Uint8Array(storedEncryptedData.split(',')), key)
-  //            .then(decrypted => {
-  //             //  setData(decrypted);
-  //             console.log(decrypted);
-  //            })
-  //            .catch(error => console.error('Erreur de déchiffrement :', error));
-  //           }
   return (
     <div>
       <Header articlesPanier={articlesPanier} setArticlesPanier={setArticlesPanier}  calcQuantity={calcQuantity} orderId={orderId} setOrderId={setOrderId} calcPrice={calcPrice} countItem={countItem} setCountItem={setCountItem} price={price} setPrice={setPrice} noItems={noItems} setNoItems={setNoItems} result={result} setResult={setResult}/>
